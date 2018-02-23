d13ddc846bfa3a03b885623a052f18825b622dd1
jihuaqiang <jihuaqiang@etongdai.com>
2018-02-23 15:00:03 +0800

    marquee test

---
+++ src/components/marquee.vue
@@ -0,0 +1,3 @@
+<template>
+  <marquee>this is marquee</marquee>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Marquee from '@/components/marquee'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Marquee',
+      component: Marquee
     }
   ]
 })
